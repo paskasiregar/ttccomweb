2401d5cf4161b54e91a49ce91eae1c13df488ecb
paskasiregar <paska.siregar@gmail.com>
2024-11-16 18:26:34 +0700

    16nov-v1-UI adjustments

---
+++ .history/src/app/layout_20241116182502.js
@@ -0,0 +1,15 @@
+import { DM_Sans } from "./next/font/google";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-dm-sans",
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={dmSans.variable}>{children}</body>
+    </html>
+  );
+}
